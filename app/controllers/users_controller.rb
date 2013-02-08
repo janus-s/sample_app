79f8b5ac31d82f8bbd765ad541c9b2e2bde56488
Janusz Smogorzewski <janusz.smogorzewski@in4mates.com>
2013-02-08 21:05:52 +0100

    modification for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
-  	  flash[:seccess] = "Welcom to the Sample App!"
+  	  flash[:success] = "Welcome to the Sample App!"
   	  redirect_to @user
   	else
   	  render 'new'
